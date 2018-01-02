14d6988bc9f8bc4712cd26620cb52e100ed54eb5
Erlinis Quintana <erlinis.quintana@gmail.com>
2018-01-02 00:05:28 -0500

    Adds book reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    {title: 'La jaula de cristal', author: 'Colin Wilson'},
+    {title: 'Orgullo y prejuicio', author: 'Jane Austen'},
+    {title: 'Brand new world', author: 'Aldous Huxley'},
+    {title: 'React and Redux', author: 'Stephen Grider'}
+  ]
+}
